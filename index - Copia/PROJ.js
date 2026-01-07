60855e42564e48436831398f9c4d6f0eae1e1924
Lara Santiago <laradeelaros@gmail.com>
2026-01-07 11:57:00 -0300

    Finaliza estrutura correta do projeto para GitHub Pages

--- index - Copia/PROJ.js
+++
@@ -1,9 +0,0 @@
-//window.alert("Seja bem-vindo(a) ao Quiz para Devs")
-    
-//    let nome = prompt("Qual o seu nome?");
- //   window.confirm ("Olá " + nome + "!!  Só um aviso: Na minha maquina funciona, se der erro é problema do seu dispositivo");
-//    let blocos = document.querySelectorAll(".nomeUsuario");
-//        blocos.forEach(el => {
-//    el.innerText = nome;
-//});
-    
